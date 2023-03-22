94c6cc20f2cbf51b442a65cb2f8419342f87f35f
ychae1997 <ychae1997@gmail.com>
2023-03-22 22:15:18 +0900

    react-refresh-webpack-plugin 설치

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 const { merge } = require('webpack-merge');
 const baseConfig = require('./webpack.config.base');
 const path = require('path');
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = merge(baseConfig, {
   mode: 'development',
@@ -13,7 +14,9 @@ module.exports = merge(baseConfig, {
   },
   optimization: {
     runtimeChunk: 'single',
-  }
+  },
+  plugins: [new ReactRefreshPlugin()],
 });
 
 // hot : true - 웹팩으로 빌드한 결과물 실시간으로 반영
+// react-refresh-webpack-plugin : webpack-dev-server처럼 저장할때마다 변경사항을 적용시며주고, 리액트의 상태를 유지해주는 플러그인
