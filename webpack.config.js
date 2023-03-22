28f14e1531bfd139b2b93844cc8d31d98b80a6e0
ychae1997 <ychae1997@gmail.com>
2023-03-22 20:58:41 +0900

    css-loader 설치 및 설정

--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,11 @@ module.exports = {
             ]
           }
         }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
       }
     ]
   }
